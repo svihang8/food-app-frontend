8f667b796bb3431406d14b2fbc70452df0bb37b1
Vihang Shah <83380615+svihang8@users.noreply.github.com>
2023-09-17 21:09:07 -0400

    added frontend code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+import Signup from './Components/Login Page/Signup';
+import Home from './Components/Home Page/Home';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from './Components/Home Page/NavBar';
+import Footer from './Components/Home Page/Footer';
+import Login from './Components/Login Page/Login';
+import ForgetPassword from './Components/Login Page/ForgetPassword';
+import ResetPassword from './Components/Login Page/ResetPassword';
+import AllPlans from './Components/Plan Page/AllPlans';
+import AuthProvider from './Components/Context/AuthProvider';
+import Profile from './Components/Profile Page/Profile';
+import PlanDetail from './Components/PlanDetail Page/PlanDetail';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider>
+        <NavBar />
+        <Switch>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/forgetPassword">
+            <ForgetPassword />
+          </Route>
+          <Route path="/resetpassword">
+            <ResetPassword />
+          </Route>
+          <Route path="/allPlans">
+            <AllPlans />
+          </Route>
+          <Route path="/profilePage">
+            <Profile />
+          </Route>
+          <Route path="/planDetail/:id">
+            <PlanDetail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </AuthProvider>
+    </Router>
   );
 }
 
